dff7b69c47c8d663ad0234bb1830b14d84c2ed31
Jack <wzolleis@web.de>
2021-11-28 16:50:26 +0100

    Zwischenstand

--- nwt-ui/src/domain/cities/components/CityCard.tsx
+++ nwt-ui/src/domain/cities/components/CityCard.tsx
@@ -14,19 +14,60 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import {ClassNameMap, makeStyles} from "@mui/styles";
+import {AppTheme} from "app/components/App";
 
 export interface CityCardProps {
     city: City
 }
 
+const useStyles = makeStyles(
+    {
+        parentFlexRight: {
+            display: "flex",
+            justifyContent: "flex-end"
+        },
+        leftAlignItem: {
+            marginRight: "auto"
+        },
+    }
+)
+
+interface CityCardActionsProps {
+    classes: ClassNameMap<"parentFlexRight" | "leftAlignItem">
+    handleExpandClick: () => void
+    expanded: boolean
+}
+
+const CityCardActions = ({classes, handleExpandClick, expanded}: CityCardActionsProps ) => {
+    return (
+        <CardActions disableSpacing className={classes.parentFlexRight}>
+            <IconButton className={classes.leftAlignItem}>
+                <FavoriteIcon/>
+            </IconButton>
+            <IconButton onClick={handleExpandClick}>
+                {expanded ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+            </IconButton>
+        </CardActions>
+    )
+}
+
 const CityCard = (props: CityCardProps) => {
     const [expanded, setExpanded] = React.useState(false);
+
+    const classes: ClassNameMap<"parentFlexRight" | "leftAlignItem"> = useStyles()
+
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
 
+    const handleMoreActionsClick = () => {
+
+    }
+
     const {city} = props
+
 
     return (
         <Card>
@@ -37,7 +78,7 @@ const CityCard = (props: CityCardProps) => {
                     </Avatar>
                 }
                 action={
-                    <IconButton aria-label="settings">
+                    <IconButton aria-label="settings" onClick={handleMoreActionsClick}>
                         <MoreVertIcon/>
                     </IconButton>
                 }
@@ -49,40 +90,18 @@ const CityCard = (props: CityCardProps) => {
                     {city.details}
                 </Typography>
             </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
+            <CardActions disableSpacing className={classes.parentFlexRight}>
+                <IconButton className={classes.leftAlignItem}>
+                    <FavoriteIcon/>
                 </IconButton>
-                <IconButton aria-label="show details" onClick={handleExpandClick}>
+                <IconButton onClick={handleExpandClick}>
                     {expanded ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
                 </IconButton>
             </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
-                    <Typography paragraph>Method:</Typography>
                     <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                        aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                        occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo in the pan. Add
-                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                        stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
-                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                        mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don’t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
+                        More Info
                     </Typography>
                 </CardContent>
             </Collapse>
