6bade4f2fab3b4b8e909d38ed9e439054393c4b3
Jack <wzolleis@web.de>
2022-01-23 22:38:04 +0100

    Text aus messages verwenden

--- nwt-ui/src/features/player/components/PlayersView.tsx
+++ nwt-ui/src/features/player/components/PlayersView.tsx
@@ -3,6 +3,7 @@ import {useAppSelector} from "app/state/hooks";
 import {selectData} from "features/data/state/dataSlice";
 import {selectSelection} from "features/data/state/selectionSlice";
 import selectionService from "features/selection/service/selectionService";
+import {messages} from "common/i18n/messages";
 
 export const PlayersView = () => {
     const {user} = useAppSelector(selectData)
@@ -10,6 +11,6 @@ export const PlayersView = () => {
     const selectedData = selectionService.selectedData(user, selection)
 
     return (
-        <div>{`Spieler: ${selectedData.player?.name || 'Kein Spieler ausgewählt'}`}</div>
+        <div>{`Spieler: ${selectedData.player?.name || messages.common.noSelection}`}</div>
     )
 }
