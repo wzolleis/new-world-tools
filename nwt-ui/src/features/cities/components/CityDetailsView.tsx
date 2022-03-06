d1060203d16b354854350906c7d5a75ce70e57ad
Jack <wzolleis@web.de>
2022-03-06 18:55:52 +0100

    item action handler als Klasse

--- nwt-ui/src/features/cities/components/CityDetailsView.tsx
+++ nwt-ui/src/features/cities/components/CityDetailsView.tsx
@@ -1,64 +1,16 @@
-import {AppBar, Grid, Toolbar, Typography} from "@mui/material";
-import {City, CityStorage, Item, Undefined} from "common/types/commonTypes";
-import React, {useState} from "react";
+import {Grid, Typography} from "@mui/material";
+import {City, CityStorage} from "common/types/commonTypes";
+import React from "react";
 import {messages} from "common/i18n/messages";
 import ItemTable from "features/storage/components/ItemTable";
-import {ItemActionHandler} from "features/cities/components/CitiesView";
-import ItemEditor, {ItemFormData} from "features/storage/components/ItemEditor";
-import {v4 as uuidv4} from "uuid";
-import Box from "@mui/material/Box";
-import AppBarAction from "common/appbar/AppBarAction";
 
 export interface CityDetailsViewProps {
     city: City | undefined
-    storage: CityStorage
+    storage: CityStorage,
 }
-
-interface ItemEditorParam {
-    item: Item
-    title: string
-}
-
-const newItem: Item = {
-    category: 'resource',
-    attributes: {},
-    quantity: 0,
-    name: '',
-    key: uuidv4()
-}
-
-const itemEditorParamNew: ItemEditorParam = {
-    item: newItem, title: messages.itemEditor.create.title
-}
-
 
 const CityDetailsView = ({city, storage}: CityDetailsViewProps) => {
-    const [itemEditorVisible, setItemEditorVisible] = useState(false);
-    const [itemEditorParam, setItemEditorParam] = useState<ItemEditorParam>(itemEditorParamNew)
-    // const actionHandler = useContext(ItemActionContext)
     if (!city) return null
-
-    const itemActionCallbacks: ItemActionHandler = {
-        onAddItem: () => {
-            console.log('TODO: add item')
-            setItemEditorVisible(true)
-        },
-        onEditItem: () => {
-            console.log('edit item')
-        },
-        onDeleteItem: () => {
-            console.log('delete item')
-        },
-        onCancel: () => {
-            setItemEditorVisible(false)
-        },
-        onSubmit: (values: ItemFormData) => {
-            setItemEditorVisible(false)
-        },
-        onSelect: (item: Undefined<Item>) => {
-            console.log('item selected', item)
-        }
-    }
 
     return (
         <div>
@@ -70,22 +22,9 @@ const CityDetailsView = ({city, storage}: CityDetailsViewProps) => {
                 </Grid>
 
                 <Grid item xs={6} sm={6}>
-                    <ItemTable storage={storage} city={city} actionHandler={itemActionCallbacks}/>
+                    <ItemTable storage={storage} city={city}/>
                 </Grid>
             </Grid>
-
-            <ItemEditor item={itemEditorParam.item} title={itemEditorParam.title} editorOpen={itemEditorVisible}
-                        actionHandler={itemActionCallbacks}/>
-
-            <AppBar position="fixed" sx={{top: 'auto', bottom: 0}} color='primary'>
-                <Toolbar>
-                    <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', flexGrow: 1}}>
-                        <AppBarAction label={messages.citiesItemsTable.actions.add} icon={"Storage"}
-                                      callback={itemActionCallbacks.onAddItem}
-                        />
-                    </Box>
-                </Toolbar>
-            </AppBar>
         </div>
     )
 }
--- nwt-ui/src/features/storage/components/StoragesView.tsx
+++ nwt-ui/src/features/storage/components/StoragesView.tsx
@@ -5,12 +5,10 @@ import * as React from "react";
 import {useState} from "react";
 import ItemSummaryTable, {ItemSummaryTableRow} from "features/storage/components/ItemSummaryTable";
 import TopAppBar from "common/components/TopAppBar";
-import {AppBar, Toolbar} from "@mui/material";
+import {Toolbar} from "@mui/material";
 import {selectCity} from "features/cities/state/citiesSlice";
 import {selectStorage} from "features/storage/state/storageSlice";
 import AppBarTitle from "common/components/AppBarTitle";
-import Box from "@mui/material/Box";
-import AppBarAction from "common/appbar/AppBarAction";
 
 const StoragesView = () => {
     const {cities} = useAppSelector(selectCity)
@@ -21,9 +19,6 @@ const StoragesView = () => {
     }
 
     const itemSummaryText = selectedRow ? `${selectedRow.name}: ${selectedRow.totalQuantity}` : messages.citiesItemsTable.noSelection
-    const onAddItem = () => {
-        console.log('add item')
-    }
 
     return (
         <>
@@ -38,16 +33,6 @@ const StoragesView = () => {
                     <ItemSummaryTable cities={cities} storages={storages} onRowSelected={onUpdateItemSelection}/>
                 </Grid>
             </Grid>
-
-            <AppBar position="fixed" sx={{top: 'auto', bottom: 0}} color='primary'>
-                <Toolbar>
-                    <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', flexGrow: 1}}>
-                        <AppBarAction label={messages.citiesItemsTable.actions.add} icon={"Storage"}
-                                      callback={onAddItem}
-                        />
-                    </Box>
-                </Toolbar>
-            </AppBar>
         </>
     )
 }
