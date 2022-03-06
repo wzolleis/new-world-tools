b6fc4ed5e3a94f3627dea2df7396d630bf7da8e5
Jack <wzolleis@web.de>
2022-03-06 14:03:47 +0100

    Item Editor in die Details verschoben. Add Item in der Storage-View

--- nwt-ui/src/features/cities/components/CityDetailsView.tsx
+++ nwt-ui/src/features/cities/components/CityDetailsView.tsx
@@ -1,18 +1,64 @@
-import {Grid, Typography} from "@mui/material";
-import {City, CityStorage} from "common/types/commonTypes";
-import React, {useContext} from "react";
+import {AppBar, Grid, Toolbar, Typography} from "@mui/material";
+import {City, CityStorage, Item, Undefined} from "common/types/commonTypes";
+import React, {useState} from "react";
 import {messages} from "common/i18n/messages";
-import CityItemTable from "features/storage/components/CityItemTable";
-import {ItemActionContext} from "features/cities/components/CitiesView";
+import ItemTable from "features/storage/components/ItemTable";
+import {ItemActionHandler} from "features/cities/components/CitiesView";
+import ItemEditor, {ItemFormData} from "features/storage/components/ItemEditor";
+import {v4 as uuidv4} from "uuid";
+import Box from "@mui/material/Box";
+import AppBarAction from "common/appbar/AppBarAction";
 
 export interface CityDetailsViewProps {
     city: City | undefined
     storage: CityStorage
 }
 
+interface ItemEditorParam {
+    item: Item
+    title: string
+}
+
+const newItem: Item = {
+    category: 'resource',
+    attributes: {},
+    quantity: 0,
+    name: '',
+    key: uuidv4()
+}
+
+const itemEditorParamNew: ItemEditorParam = {
+    item: newItem, title: messages.itemEditor.create.title
+}
+
+
 const CityDetailsView = ({city, storage}: CityDetailsViewProps) => {
-    const actionHandler = useContext(ItemActionContext)
+    const [itemEditorVisible, setItemEditorVisible] = useState(false);
+    const [itemEditorParam, setItemEditorParam] = useState<ItemEditorParam>(itemEditorParamNew)
+    // const actionHandler = useContext(ItemActionContext)
     if (!city) return null
+
+    const itemActionCallbacks: ItemActionHandler = {
+        onAddItem: () => {
+            console.log('TODO: add item')
+            setItemEditorVisible(true)
+        },
+        onEditItem: () => {
+            console.log('edit item')
+        },
+        onDeleteItem: () => {
+            console.log('delete item')
+        },
+        onCancel: () => {
+            setItemEditorVisible(false)
+        },
+        onSubmit: (values: ItemFormData) => {
+            setItemEditorVisible(false)
+        },
+        onSelect: (item: Undefined<Item>) => {
+            console.log('item selected', item)
+        }
+    }
 
     return (
         <div>
@@ -24,9 +70,22 @@ const CityDetailsView = ({city, storage}: CityDetailsViewProps) => {
                 </Grid>
 
                 <Grid item xs={6} sm={6}>
-                    <CityItemTable storage={storage} city={city} actionHandler={actionHandler}/>
+                    <ItemTable storage={storage} city={city} actionHandler={itemActionCallbacks}/>
                 </Grid>
             </Grid>
+
+            <ItemEditor item={itemEditorParam.item} title={itemEditorParam.title} editorOpen={itemEditorVisible}
+                        actionHandler={itemActionCallbacks}/>
+
+            <AppBar position="fixed" sx={{top: 'auto', bottom: 0}} color='primary'>
+                <Toolbar>
+                    <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', flexGrow: 1}}>
+                        <AppBarAction label={messages.citiesItemsTable.actions.add} icon={"Storage"}
+                                      callback={itemActionCallbacks.onAddItem}
+                        />
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
