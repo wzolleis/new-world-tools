9c340dc646baae4fcadcfbd75c2ef62f4ab2908b
Jack <wzolleis@web.de>
2022-07-31 00:24:10 +0200

    WelcomeView.tsx auf styled umgestellt

--- nwt-ui/src/common/components/WelcomeView.tsx
+++ nwt-ui/src/common/components/WelcomeView.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
 import {messages} from "common/i18n/messages";
 import {Toolbar, Typography} from "@mui/material";
-import {makeStyles} from "@mui/styles";
 import LayoutConstants from "common/constants/layoutConstants";
 import TopAppBar from "common/components/TopAppBar";
-import {AppTheme} from "app/components/appTheme";
+import {styled} from "@mui/material/styles";
 
-const useStyles = makeStyles((_: AppTheme) => {
-    const {drawerWidth} = LayoutConstants
-
-    return {
-        title: {
-            marginLeft: drawerWidth
-        },
-    }
-})
-
+const {drawerWidth} = LayoutConstants
+const Root = styled('div')(() => ({
+    display: 'flex',
+    marginLeft: drawerWidth,
+}))
 
 const WelcomeView = () => {
-    const classes = useStyles()
     return (
         <>
             <TopAppBar>
                 <Toolbar>
-                    <div className={classes.title}>
+                    <Root>
                         <Typography>{messages.welcomePage.appBarTitle}</Typography>
-                    </div>
+                    </Root>
                 </Toolbar>
             </TopAppBar>
 
