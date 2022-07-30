6e6d7d5ae74b6c312662fe69e44a89b28e38c1af
Jack <wzolleis@web.de>
2022-07-31 00:21:19 +0200

    TopAppBar.tsx umgestellt

--- nwt-ui/src/common/components/TopAppBar.tsx
+++ nwt-ui/src/common/components/TopAppBar.tsx
@@ -1,29 +1,13 @@
 import {AppBar} from "@mui/material";
 import React, {FunctionComponent, PropsWithChildren} from "react";
-import {makeStyles} from "@mui/styles";
-import LayoutConstants from "common/constants/layoutConstants";
-import {AppTheme} from "app/components/appTheme";
 
-const useStyles = makeStyles((theme: AppTheme) => {
-    const {drawerWidth} = LayoutConstants
-    return {
-        header: {
-            width: `calc(100% - ${drawerWidth}px)`,
-        },
-    }
-})
-
-export interface AppBarProps {
-}
-
-export type AppBarContainerProps = PropsWithChildren<AppBarProps>
+export type AppBarContainerProps = PropsWithChildren<{}>
 
 const TopAppBar: FunctionComponent<AppBarContainerProps> = ({children}: AppBarContainerProps) => {
-    const classes = useStyles()
     return (
-        <AppBar className={classes.header}
-                position='fixed'
-                elevation={0}
+        <AppBar
+            position='fixed'
+            elevation={0}
         >
             {children}
         </AppBar>
